c6af49a29727cbabb6eaa41958bcb7a0f8e9fa27
jmac <jason.mac.laptop@gmail.com>
2025-03-09 19:55:58 -0700

    Update "about" page.

--- src/routes/pages/settings/about/AboutPage.tsx
+++ src/routes/pages/settings/about/AboutPage.tsx
@@ -96,88 +96,13 @@ export default function AboutPage() {
           <IonTitle>About</IonTitle>
         </IonToolbar>
       </AppHeader>
+
       <AppContent scrollY fullscreen>
+
         <AppDetails />
 
-        <IonList inset color="primary">
-          <IonItemInAppExternalLink
-            href="https://github.com/aeharding/voyager/releases"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBg color="color(display-p3 0.7 0 1)" size="0.8" slot="start">
-              <IonIcon icon={sparkles} />
-            </IconBg>
-            <IonLabel>What&apos;s new</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink
-            routerLink={buildGeneralBrowseLink(`/c/${appCommunityHandle}`)}
-            detail
-          >
-            <IconBg color="color(display-p3 0 0.8 0.3)" slot="start">
-              <IonIcon icon={people} />
-            </IconBg>
-            <IonLabel>VoyagerApp Community</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink
-            detail
-            href="https://social.harding.dev/@alex"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBg color="#563acc" slot="start">
-              <IonIcon icon={logoMastodon} />
-            </IconBg>
-            <IonLabel>@alex@harding.dev</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink
-            detail
-            href={VOYAGER_PRIVACY}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBg color="#0e7afe" slot="start">
-              <IonIcon icon={lockClosed} />
-            </IconBg>
-            <IonLabel>Privacy Policy</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink
-            detail
-            href={VOYAGER_TERMS}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBg color="#8e8700" slot="start">
-              <IonIcon icon={handLeft} />
-            </IconBg>
-            <IonLabel>Terms of Use</IonLabel>
-          </IonItemInAppExternalLink>
-          {rateVoyager}
-          <IonItemInAppExternalLink detail routerLink="/settings/about/thanks">
-            <IconBg color="color(display-p3 0.1 0.6 0.1)" slot="start">
-              <IonIcon icon={thumbsUp} />
-            </IconBg>
-            <IonLabel>Thanks To</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink
-            detail
-            href="https://github.com/aeharding/voyager/issues"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconBg color="color(display-p3 0.5 0.7 0.1)" slot="start">
-              <IonIcon icon={bug} />
-            </IconBg>
-            <IonLabel>Bug Tracker</IonLabel>
-          </IonItemInAppExternalLink>
-          <IonItemInAppExternalLink detail onClick={getCompliment}>
-            <IconBg color="color(display-p3 1 0.1 0.6)" size="1.1" slot="start">
-              <IonIcon icon={happy} />
-            </IconBg>
-            <IonLabel>Get a Compliment</IonLabel>
-          </IonItemInAppExternalLink>
-        </IonList>
       </AppContent>
+
     </IonPage>
   );
 }
--- src/routes/pages/settings/about/AppDetails.tsx
+++ src/routes/pages/settings/about/AppDetails.tsx
@@ -2,14 +2,56 @@ import AppVersionInfo from "./AppVersionInfo";
 
 import styles from "./AppDetails.module.css";
 
+import { IonItemInAppExternalLink } from "#/features/shared/InAppExternalLink";
+
 export default function AppDetails() {
   return (
-    <div className={styles.container}>
-      <img src="/logo.png" alt="" />
-      <div>
-        Voyager <AppVersionInfo verbose betaAs="aside" />
-        <aside>by Alexander Harding</aside>
+
+    <>
+
+      <div className={styles.container}>
+
+        <img src="/logo.png" alt="" />
+
+        <div>
+            Sea Voyager
+        </div>
+
       </div>
-    </div>
+
+      <div>
+        <p>
+
+          Sea Voyager is a fork of Voyager.
+
+        </p>
+
+        <p>
+
+          <IonItemInAppExternalLink
+            detail
+            href="https://github.com/aeharding/voyager"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The official Voyager app by Alexander Harding can be found here.
+          </IonItemInAppExternalLink>
+
+        </p>
+
+        <p>
+            Sea Voyager helps you to see Voyager with 
+            enhanced large text support.
+        </p>
+
+        <p>
+            The hope is that these accessibility enhancements can be
+            merged into or otherwise supported by the official
+            Voyager app.
+        </p>
+
+      </div>
+ 
+    </>
   );
 }
