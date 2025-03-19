ca8f814719b7fabedba00e50d14f3a867a7547d9
jmac <jason.mac.laptop@gmail.com>
2025-03-19 00:08:47 -0700

    Automate build version update.

--- build-ios.sh
+++ build-ios.sh
@@ -3,6 +3,34 @@
 # Build ios app
 #
 
+#
+# Get current version. Increment current minor version.
+#
+MAJOR_VERSION=$(cat version.txt | sed 's/\.[0-9]*$//')
+MINOR_VERSION=$(cat version.txt | sed 's/[0-9]\.[0-9].//')
+MINOR_VERSION=$((${MINOR_VERSION} + 1))
+
+FULL_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}"
+
+echo ${FULL_VERSION} > version.txt
+
+#
+# Inject new version number into build files and app details page.
+#
+
+cat ios/App/App.xcodeproj/project.pbxproj.template | \
+    sed "s/__INJECT_VERSION/${FULL_VERSION}/g" > \
+    ios/App/App.xcodeproj/project.pbxproj
+
+cat src/routes/pages/settings/about/AppDetails.tsx.template | \
+    sed "s/__INJECT_VERSION/${FULL_VERSION}/g" > \
+    src/routes/pages/settings/about/AppDetails.tsx
+
+
+#
+# Start build
+#
+
 export BUILD_FOSS_ONLY=true
 
 corepack enable
